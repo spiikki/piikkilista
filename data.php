a163d606c655d6003b7a285ad4d671cc1c0bf6f7
spiikki sillanpää <spiikki@nalleperhe.net>
2012-09-21 00:02:32 +0300

    moved account change to data.php now posting bought amount (easier for cart)
    user accountValue loading functionality

--- data.php
+++ data.php
@@ -2,21 +2,32 @@
 
 // load storage
 $data = file('storage.json');
-$storage = json_decode($data[0],true);
+$storage = json_decode($data[0], true);
 
-//update accountValue
-foreach($storage as $key=>$value) {
-	if($key == $_POST['user']) {
-		$storage[$key]['accountValue'] = floatval($_POST['accountValue']);
-		// return new value of account
-		echo $storage[$key]['accountValue'];
+if (isset($_POST['amount'])) {
+	//update accountValue
+	foreach ($storage as $key => $value) {
+		if ($key == $_POST['user']) {
+			$newValue = $value['accountValue'] - floatval($_POST['amount']);
+			$storage[$key]['accountValue'] = $newValue;
+			// return new value of account
+			echo $storage[$key]['accountValue'];
+		}
 	}
+
+	// save storage
+	$file = fopen('storage.json', 'w');
+	fwrite($file, json_encode($storage));
+	fclose($file);
+
+} else {
+	//return users accountValue
+	foreach ($storage as $key => $value) {
+		if ($key == $_POST['user']) {
+			echo $storage[$key]['accountValue'];
+		}
+	}
+	
 }
-
-// save storage
-$file = fopen('storage.json','w');
-fwrite($file,json_encode($storage));
-fclose($file);
-
 
 ?>
